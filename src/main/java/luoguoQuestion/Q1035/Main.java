5fcd45dccd4741125de2cbfafc31105e7a750da4
astoria <2765036345@qq.com>
2021-03-19 15:22:16 +0800

    add.

--- src/main/java/luoguoQuestion/Q1035/Main.java
+++ src/main/java/luoguoQuestion/Q1035/Main.java
@@ -1,24 +1,11 @@
 package luoguoQuestion.Q1035;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 9de204fb1f4460be8c3acbb4beb97228375df97d
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-<<<<<<< HEAD
-        int k = scanner.nextInt();
-        double  sum = 0;
-        int result = 1;
-        while(sum < k){
-            sum += 1L/(double)result;
-            result++;
-        }
-        System.out.println(result-1);
-=======
         int number = scanner.nextInt();
         double sum = 0;
         int result = 0;
@@ -27,7 +14,5 @@ public class Main {
             result++;
         }
         System.out.println(result);
-
->>>>>>> 9de204fb1f4460be8c3acbb4beb97228375df97d
     }
 }
