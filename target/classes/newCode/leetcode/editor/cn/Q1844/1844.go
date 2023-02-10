c38ff77aefa47c1fb1869d299875e2d870fbfb7c
ASTORIA <2765036345@qq.com>
2023-02-10 06:12:11 +0000

    ad.

---
+++ target/classes/newCode/leetcode/editor/cn/Q1844/1844.go
@@ -0,0 +1,8 @@
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func replaceDigits(s string) string {
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
